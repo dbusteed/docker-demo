29d1bc350eaee705b2c6e6107020412e9bb45dc3
Davis Busteed <davisrb@byu.local>
2018-03-30 11:15:44 -0600

    start

---
+++ complete/dmp/homepage/views/index.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django_mako_plus import view_function, jscontext
+from datetime import datetime, timezone
+from brule import randomName
+
+@view_function
+def process_request(request):
+    utc_time = datetime.utcnow()
+    context = {
+        # sent to index.html:
+        'utc_time': utc_time,
+        # sent to index.html and index.js:
+        jscontext('utc_epoch'): utc_time.timestamp(),
+        'name': randomName()
+    }
+    return request.dmp.render('index.html', context)
